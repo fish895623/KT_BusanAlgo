22abbf01a3fe83540e5b7de51b9d383508b30699
jongwon Kim <jong960807@gmail.com>
2021-12-23 23:57:00 +0900

    1874번 수정

--- 12/23/Boj_1874번_김종원.py
+++ 12/23/Boj_1874번_김종원.py
@@ -20,27 +20,55 @@ for i in range(n):
     numbox.append(i+1)
     
 findnum = findnums.pop(0)
+Flag = True
+commands = []
 while findnums:
     while numbox:
         n = numbox.pop(0)
         if n < findnum:
             numboxs.append(n)
-            print("push")
+            # print("push")
+            commands.append("+")
         elif n == findnum:
             numboxs.append(n)
-            print("push")
+            # print("push")
+            commands.append("+")
             break
         else:
-            print("No")
-    while True:
-        if numboxs[-1] == findnum:
-            numboxs.pop()
-            print("pop")
-            if len(findnums) ==0:
+            Flag = False
+            break
+    if Flag:
+        while numboxs:
+            if numboxs[-1] == findnum:
+                numboxs.pop()
+                # print("pop")
+                commands.append("-")
+                if len(findnums) ==0:
+                    break
+                else:
+                    findnum = findnums.pop(0)
+            elif numboxs[-1] > findnum:
+                Flag = False
                 break
-            else:
-                findnum = findnums.pop(0)
-        elif numboxs[-1] != findnum:
-            break
-        elif len(numboxs) ==0:
-            break
+            elif numboxs[-1] != findnum:
+                break
+            elif len(numboxs) ==0:
+                break
+    if not(Flag):
+        break
+if not(Flag):
+    print("No")
+else:
+    for k in commands:
+        print(k)
+
+
+# 5
+# n1 = 1 2 3 4 5
+# answer = 1 2 5 3 4
+# push pop push pop push push pop 
+
+# n2 = 
+# 3 4 
+
+# 
