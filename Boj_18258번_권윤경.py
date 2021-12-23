c851071a0ea2392a41e06a3327cc7d3f5ad1ec91
yoonkyeong <dbsrud5916@gmail.com>
2021-12-24 00:58:59 +0900

    18258수정

---
+++ Boj_18258번_권윤경.py
@@ -0,0 +1,44 @@
+from sys import stdin
+n = int(stdin.readline())
+s = []
+for i in range(n):
+	s.append(stdin.readline())
+    
+from collections import deque
+
+queue = deque()
+
+for i in range(n):
+    if s[i].split()[0] == 'push':
+        queue.append(s[i].split()[1])
+        
+    elif s[i].split()[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            p = queue.popleft()
+            print(p)
+        
+    elif s[i].split()[0] == 'size':
+        print(len(queue))
+        
+    elif s[i].split()[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif s[i].split()[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+        
+    elif s[i].split()[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
+        
+    else:
+        break
