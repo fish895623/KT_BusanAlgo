58a718757f019826a6663c96be6b0a0518a31fa4
fish895623 <dan990429@gmail.com>
2021-12-27 23:55:58 +0900

    input data

---
+++ 12/27/boj_11725번_배성훈.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    N = int(input())
+
+    num = []
+
+    for i in range(N):
+        a, b = input().rstrip().rsplit()
+
+    for i in range(num):
+        for a, b in i:
+            print(a, b)
---
+++ 12/27/boj_4256번_배성훈.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int pre[1001], in[1001], inFrom[1001];
+int rootIdx;
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+  // 반복횟수
+  while (t--) {
+    int n;
+    // node 길이
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) scanf("%d", pre + i)
+  }
+
+  return 0;
+}
