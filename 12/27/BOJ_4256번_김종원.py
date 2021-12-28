a72b91b2875f8953c0e4dafc5566567d9760e459
jongwon Kim <jong960807@gmail.com>
2021-12-28 20:43:29 +0900

    수정본

--- 12/27/BOJ_4256번_김종원.py
+++ 12/27/BOJ_4256번_김종원.py
@@ -1,35 +1,14 @@
-# class Nodee:
-#     def __init__(self,x):
-#         self.current = x
-#         self.left = 0
-#         self.right = 0
-#     def leftNode(self,fx):
-#         self.left = fx
-#     def rightNode(self,rx):
-#         self.right = rx
+# BOJ_4256번
 
-# no = Nodee()
-# node = int(input())
+def pre_in_to_post(pre_list,in_list):
+    if pre_list:
+        root = pre_list[0]
+        mid = in_list.index(root)
+        pre_in_to_post(pre_list[1:mid+1], in_list[:mid])
+        pre_in_to_post(pre_list[mid+1 :], in_list[mid+1:])
+        print(root, end=" ")
 
-node = 4
-mid = [3,2,1,4]
-front = [2,3,4,1]
+pre_order = [3,2,1,4]
+in_order = [2,3,4,1]
 
-root = [mid[0]]
-right_root = front[-1]
-
-left_tree  = []
-right_tree = []
-for x in range(len(front)):
-    if front[x] == root[0]:
-        break
-    left_tree.append(front[x])
-right_tree = front[x+1:]
-
-print(left_tree)
-print(root)
-print(right_tree)
-print(right_root)
-# if len(right_tree) !=1:
-
-
+pre_in_to_post(pre_order, in_order)
