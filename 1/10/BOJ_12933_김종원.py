ac7399e6ab119a590376ce6d4561672024529927
jongwon Kim <jong960807@gmail.com>
2022-01-11 11:23:21 +0900

    86%에서 시간초과??

---
+++ 1/10/BOJ_12933_김종원.py
@@ -0,0 +1,40 @@
+S = "quqacukqauackck"
+S = "quackquackquackquackquackquackquackquackquackquack"
+S = "qqqqqqqqqquuuuuuuuuuaaaaaaaaaacccccccccckkkkkkkkkk"
+# S = "quackqauckquack"
+# S = "quqaquuacakcqckkuaquckqauckack"
+# S = "kcauq"
+
+## 오리문제는 quack 순으로 문자열을 탐색을 진행하는데,
+## 찾아야하는 문자(findStr)과 현재 문자열이 같고 이전에 방문(visited)한적이 없다면
+### 방문 인덱스에 0에서 1로 수정하고 찾아야했던 문자열을 다시 text 리스트에 append 함으로써 순회하면서 탐색이 가능하게 하였다.
+### 처음 q 문자(findStr)을 탐색할때, text 리스트 원소에는 [u,a,c,k]가 존재하고 S 문자열에서 순차적으로 q문자열을 찾았을때 방문하지 않았다면
+### 방문(visited) 변수 인덱스에 방문했다고 1로 수정하고 text 리스트 원소에 q를 append 함으로써 [u,a,c,k,q]가 된다. 그 후 findStr 값을 u로 업데이트 시키면서
+### 순차적으로 탐색을 한다. 그러면서 searchingCount를 이용해 탐색 횟수를 카운트를 해주면서 quack의 문자열 길이 5로 나눔으로써 나머지가 0으로 떨어지지 않는다면
+### quack라는 문자열이 전부 탐색이 되지 않은 경우이기에 -1을 리턴한다.
+
+import sys
+
+S = sys.stdin.readline().rstrip()
+vistied = [0] * len(S)
+count = 0
+flag = True
+
+while sum(vistied) != len(S):
+    text = list("quack")
+    findStr = text.pop(0)
+    searchingCount =0
+    for idx, txt in enumerate(S):
+        if (findStr == txt and not(vistied[idx])):
+            vistied[idx] = 1
+            text.append(findStr)
+            findStr = text.pop(0)
+            searchingCount +=1
+    if searchingCount % 5 !=0:
+        flag = False
+        break
+    count +=1
+if flag:
+    print(count)
+else:
+    print(-1)
