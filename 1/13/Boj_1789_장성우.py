5083518163562d9a95a89447fe07ee8393a07c58
CastleRain <jjangsw12@gmail.com>
2022-01-13 22:19:38 +0900

    0113 성우

---
+++ 1/13/Boj_1789_장성우.py
@@ -0,0 +1,20 @@
+"""
+https://www.acmicpc.net/problem/1789
+
+200
+-> 1 ~ 19 sum (10제외) + 20
+
+1 ~ n까지의 합 : n(n+1)/2
+
+"""
+import math
+
+n = int(input()) * 2
+
+result = int(math.sqrt(n))
+
+while result ** 2 + result > n:
+    result -= 1
+
+print(result)
+
