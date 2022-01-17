21685da3615fae185434c4510eb1aa91b99ac1a1
CastleRain <jjangsw12@gmail.com>
2022-01-17 23:47:18 +0900

    0117 성우

--- 1/17/Boj_11365_장성우.py
+++ 1/17/Boj_11365_장성우.py
@@ -2,12 +2,9 @@
 https://www.acmicpc.net/problem/11365
 """
 
-data = []
+
 while True:
     d = input()
     if d == "END":
         break
-    data.append(d[::-1])
-
-for i in data:
-    print(i)
+    print(d[::-1])
