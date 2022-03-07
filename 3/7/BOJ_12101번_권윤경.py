91923ce70a52193b8f27953e7ab70a22f2b14ae1
yoonkyeong <dbsrud5916@gmail.com>
2022-03-07 23:26:51 +0900

    3/7 윤경

---
+++ 3/7/BOJ_12101번_권윤경.py
@@ -0,0 +1,22 @@
+# 1,2,3 더하기
+# dfs
+
+def dfs(num, now_sum, arr):
+    global cnt
+    if now_sum == num:
+        cnt += 1
+        if cnt == k:
+            print("".join(arr)[:-1])
+            exit(0)
+            
+    if now_sum > num:
+        return   
+        
+    for i in range(1,4):
+        arr.append(f"{i} + ")
+        dfs(num, now_sum+i, arr)
+        arr.pop()
+
+n, k = map(int,input().split())
+cnt = 0
+dfs(n,0,[])
